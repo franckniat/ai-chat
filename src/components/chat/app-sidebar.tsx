eebd49739fb4939a80f2e8cc21e622c55b517ffa
Franck NIAT <niatmanuel2002@gmail.com>
2025-05-17 00:28:31 +0100

    config sidebar in chat layout

---
+++ src/components/chat/app-sidebar.tsx
@@ -0,0 +1,104 @@
+"use client"
+
+import * as React from "react"
+import {
+    BarChartIcon,
+    FolderIcon,
+    HelpCircleIcon,
+    LayoutDashboardIcon,
+    ListIcon,
+    SearchIcon,
+    SettingsIcon,
+    Sparkles,
+    UsersIcon,
+} from "lucide-react"
+
+import { NavMain } from "@/components/chat/nav-main"
+import { NavSecondary } from "@/components/chat/nav-secondary"
+import { NavUser } from "@/components/chat/nav-user"
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarHeader,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+} from "@/components/ui/sidebar"
+import Link from "next/link"
+
+const data = {
+    navMain: [
+        {
+            title: "Dashboard",
+            url: "#",
+            icon: LayoutDashboardIcon,
+        },
+        {
+            title: "Lifecycle",
+            url: "#",
+            icon: ListIcon,
+        },
+        {
+            title: "Analytics",
+            url: "#",
+            icon: BarChartIcon,
+        },
+        {
+            title: "Projects",
+            url: "#",
+            icon: FolderIcon,
+        },
+        {
+            title: "Team",
+            url: "#",
+            icon: UsersIcon,
+        },
+    ],
+    navSecondary: [
+        {
+            title: "Settings",
+            url: "#",
+            icon: SettingsIcon,
+        },
+        {
+            title: "Get Help",
+            url: "#",
+            icon: HelpCircleIcon,
+        },
+        {
+            title: "Search",
+            url: "#",
+            icon: SearchIcon,
+        },
+    ],
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+    return (
+        <Sidebar collapsible="offcanvas" {...props}>
+            <SidebarHeader>
+                <SidebarMenu>
+                    <SidebarMenuItem>
+                        <SidebarMenuButton
+                            asChild
+                            className="data-[slot=sidebar-menu-button]:!p-1.5"
+                        >
+                            <Link href="/chat">
+                                <Sparkles className="h-5 w-5" />
+                                <span className="text-base font-semibold">niato ai.</span>
+                            </Link>
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarHeader>
+            <SidebarContent>
+                <NavMain items={data.navMain} />
+                <NavSecondary items={data.navSecondary} className="mt-auto" />
+            </SidebarContent>
+            <SidebarFooter>
+                <NavUser />
+            </SidebarFooter>
+        </Sidebar>
+    )
+}
