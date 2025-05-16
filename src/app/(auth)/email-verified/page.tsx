c8ea356a9808a57cc1d83b1bbdedb27c481f87aa
Franck NIAT <niatmanuel2002@gmail.com>
2025-05-17 00:49:14 +0100

    wrap useSearchParams in Suspense

--- src/app/(auth)/email-verified/page.tsx
+++ src/app/(auth)/email-verified/page.tsx
@@ -1,10 +1,13 @@
 import VerificationForm from "@/components/auth/verified-email";
+import { Suspense } from "react"
 
 export default function VerifiedEmailPage() {
     return (
         <div className="flex flex-1 items-center justify-center">
             <div className="w-full max-w-xs">
-                <VerificationForm/>
+                <Suspense>
+                    <VerificationForm/>
+                </Suspense>
             </div>
         </div>
     )
