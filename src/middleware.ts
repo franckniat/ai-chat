03dddf765dce9c09c841251fc13812f1cbe95a80
Franck NIAT <niatmanuel2002@gmail.com>
2025-05-17 00:33:06 +0100

    config protected routes in the middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,45 @@
 import { NextRequest, NextResponse } from "next/server";
-import { getSessionCookie } from "better-auth/cookies";
-import { authRoutes } from "@/routes";
- 
+import { betterFetch } from "@better-fetch/fetch";
+import { Session } from "@/lib/auth";
+
+const authRoutes = ["/login", "/register", "/email-verified"];
+const passwordRoutes = ["/reset-password", "/forgot-password"];
+const protectedRoutes = ["/chat"];
+
 export async function middleware(request: NextRequest) {
-	const sessionCookie = getSessionCookie(request);
+
+	const { data: session } = await betterFetch<Session>(
+		"/api/auth/get-session",
+		{
+			baseURL: process.env.BETTER_AUTH_URL,
+			headers: {
+				//get the cookie from the request
+				cookie: request.headers.get("cookie") || "",
+			},
+		},
+	);
 	const isAuthRoute = authRoutes.includes(request.nextUrl.pathname);
- 
-	if (!sessionCookie && isAuthRoute ) {
-		return null
+	const isPasswordRoute = passwordRoutes.includes(request.nextUrl.pathname);
+	const isProtectedRoute = protectedRoutes.includes(request.nextUrl.pathname);
+
+	if (!session) {
+		if (isAuthRoute || isPasswordRoute) {
+			return NextResponse.next();
+		}
+		return NextResponse.redirect(new URL("/login", request.url));
 	}
-	if (sessionCookie && isAuthRoute) {
+
+	if (isProtectedRoute && !session.user.emailVerified) {
+		return NextResponse.redirect(new URL("/login", request.url));
+	}
+
+	if (session && isAuthRoute) {
 		return NextResponse.redirect(new URL("/chat", request.url));
 	}
+
+	return NextResponse.next();
 }
- 
+
 export const config = {
-	matcher: ["/dashboard", "/chat", "/settings", "/login", "/register"],
+	matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 };
