a01d8b9b261d42cd1a7c0dc64f2f6b42e2f9eb81
Franck NIAT <niatmanuel2002@gmail.com>
2025-05-17 00:34:15 +0100

    add env variables in next config

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,8 @@ const nextConfig: NextConfig = {
     BASE_URL: process.env.BASE_URL,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    RESEND_API_KEY: process.env.RESEND_API_KEY,
+    EMAIL_VERIFICATION_CALLBACK_URL: process.env.EMAIL_VERIFICATION_CALLBACK_URL,
   },
 };
 
