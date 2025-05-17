09771787c91714080bedc60d3c65a0241f39dca3
Franck NIAT <niatmanuel2002@gmail.com>
2025-05-17 03:10:46 +0100

    fix prisma client import

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,16 +1,10 @@
-import { PrismaClient } from "@niato-ai/prisma-client";
+// lib/prisma.ts
+import { PrismaClient } from "@prisma/client";
 
-const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
-
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
-
+const prisma =
+  globalForPrisma.prisma || new PrismaClient();
 export default prisma;
 
-// Ensure the prisma instance is reused across hot reloads in development
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
