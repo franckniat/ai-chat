94ce076299c64b06ad8ca427dc69610cb3c12690
franckniat <niatmanuel2002@gmail.com>
2025-08-06 18:27:41 +0100

    style: update chat sidebar and main navigation

--- src/components/chat/nav-main.tsx
+++ src/components/chat/nav-main.tsx
@@ -1,41 +1,50 @@
-"use client";
+'use client'
 
-import { type LucideIcon } from "lucide-react";
+import { type LucideIcon } from 'lucide-react'
 
 import {
-	SidebarGroup,
-	SidebarGroupContent,
-	SidebarMenu,
-	SidebarMenuButton,
-	SidebarMenuItem,
-} from "@/components/ui/sidebar";
-import { usePathname, useRouter } from "next/navigation";
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+    useSidebar,
+} from '@/components/ui/sidebar'
+import { usePathname, useRouter } from 'next/navigation'
 
 export function NavMain({
-	items,
+    items,
 }: {
-	items: {
-		title: string;
-		url: string;
-		icon?: LucideIcon;
-	}[];
+    items: {
+        title: string
+        url: string
+        icon?: LucideIcon
+    }[]
 }) {
-	const router = useRouter();
-	const pathname = usePathname();
-	return (
-		<SidebarGroup>
-			<SidebarGroupContent className="flex flex-col gap-2">
-				<SidebarMenu>
-					{items.map((item) => (
-						<SidebarMenuItem key={item.title}>
-							<SidebarMenuButton tooltip={item.title} isActive={pathname === item.url} className="cursor-pointer" onClick={() => router.push(item.url)}>
-								{item.icon && <item.icon />}
-								<span>{item.title}</span>
-							</SidebarMenuButton>
-						</SidebarMenuItem>
-					))}
-				</SidebarMenu>
-			</SidebarGroupContent>
-		</SidebarGroup>
-	);
+    const router = useRouter()
+    const pathname = usePathname()
+    const { state } = useSidebar()
+    return (
+        <SidebarGroup>
+            <SidebarGroupContent className="flex flex-col gap-2">
+                <SidebarMenu>
+                    {state === 'expanded' &&
+                        items.map((item) => (
+                            <SidebarMenuItem key={item.title}>
+                                <SidebarMenuButton
+                                    tooltip={item.title}
+                                    isActive={pathname === item.url}
+                                    title={item.title}
+                                    className="cursor-pointer"
+                                    onClick={() => router.push(item.url)}
+                                >
+                                    {item.icon && <item.icon />}
+                                    <span>{item.title}</span>
+                                </SidebarMenuButton>
+                            </SidebarMenuItem>
+                        ))}
+                </SidebarMenu>
+            </SidebarGroupContent>
+        </SidebarGroup>
+    )
 }
