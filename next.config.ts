8986ae8a4b2ac7012ac7e2297ef77de19faf0079
Franck NIAT <97402791+franckniat@users.noreply.github.com>
2025-08-09 16:25:40 +0100

    Staging (#37)

--- next.config.ts
+++ next.config.ts
@@ -2,23 +2,35 @@ import type { NextConfig } from "next";
 import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin';
 
 const nextConfig: NextConfig = {
-  env: {
-    DATABASE_URL: process.env.DATABASE_URL,
-    BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
-    BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
-    BASE_URL: process.env.BASE_URL,
-    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-    RESEND_API_KEY: process.env.RESEND_API_KEY,
-    EMAIL_VERIFICATION_CALLBACK_URL: process.env.EMAIL_VERIFICATION_CALLBACK_URL,
-    UPLOADTHING_TOKEN: process.env.UPLOADTHING_TOKEN,
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.plugins = [...config.plugins, new PrismaPlugin()];
-    }
-    return config;
-  },
+    env: {
+        DATABASE_URL: process.env.DATABASE_URL,
+        BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
+        BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
+        BASE_URL: process.env.BASE_URL,
+        GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+        GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+        RESEND_API_KEY: process.env.RESEND_API_KEY,
+        EMAIL_VERIFICATION_CALLBACK_URL: process.env.EMAIL_VERIFICATION_CALLBACK_URL,
+        UPLOADTHING_TOKEN: process.env.UPLOADTHING_TOKEN,
+        OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+        GITHUB_TOKEN: process.env.GITHUB_TOKEN,
+        GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+        GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+        ALGOLIA_APP_ID: process.env.ALGOLIA_APP_ID,
+        ALGOLIA_SEARCH_API_KEY: process.env.ALGOLIA_SEARCH_API_KEY,
+        ALGOLIA_ADMIN_API_KEY: process.env.ALGOLIA_ADMIN_API_KEY,
+    },
+    turbopack: {
+        rules: {
+            // Add your turbopack rules here
+        }
+    },
+    webpack: (config, { isServer }) => {
+        if (isServer) {
+            config.plugins = [...config.plugins, new PrismaPlugin()];
+        }
+        return config;
+    },
 };
 
 export default nextConfig;
