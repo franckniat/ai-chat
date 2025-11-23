bd74a01c31635a49e7ad107fde03cb3c5f8ef85b
yan <yannlekeumo5@gmail.com>
2025-11-23 16:35:08 +0100

    feat: landing page, icon at the centre

--- src/components/ui/FeatureCard.tsx
+++ src/components/ui/FeatureCard.tsx
@@ -2,14 +2,14 @@ import { Card, CardContent, CardHeader, CardTitle } from "./card";
 
 export default function FeatureCard({ icon, title, description }: { icon: React.ReactNode, title: string, description: string }) {
     return (
-        <Card className="border-none shadow-none bg-background/50 hover:bg-background/80 transition-colors">
-            <CardHeader>
-                <div className="mb-4 p-3 bg-primary/10 w-fit rounded-xl">
+        <Card className="border-none shadow-none bg-background/50 hover:bg-background/80 transition-colors ">
+            <CardHeader className="flex items-center flex-col">
+                <div className="mb-4 p-3 bg-primary/10 w-fit rounded-xl ">
                     {icon}
                 </div>
                 <CardTitle className="text-xl">{title}</CardTitle>
             </CardHeader>
-            <CardContent>
+            <CardContent >
                 <p className="text-muted-foreground leading-relaxed">
                     {description}
                 </p>
