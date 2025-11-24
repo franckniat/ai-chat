058271d97799c7873fb99cbe8cb92c76f4d7a923
dronex237 <161221328+dronex237@users.noreply.github.com>
2025-11-24 17:52:44 +0100

    Feature/page not found (#54)
    * feat: mettre à jour les exemples de contenu dans le composant MessagesContent
    
    * update messages component
    
    * add fotter to the main page
    
    * update prettier config
    
    * Staging (#36)
    
    * Staging (#37)
    
    * Preview (#38)
    
    * Update UI design and font (#39)
    
    * preview by yan
    
    * feat: desigining landing page, Hero, features,testimonials,Faq,CTA
    
    * feat: landing page, creation of component Faq,Testimonial and FeatureCard
    
    * feat: landing page, icon at the centre
    
    * Dev yan (#44)
    
    * feat: mettre à jour les exemples de contenu dans le composant MessagesContent
    
    * update messages component
    
    * Preview (#35)
    
    * update prettier config
    
    * Staging (#36)
    
    * Staging (#37)
    
    * Preview (#38)
    
    * Update UI design and font (#39)
    
    * feat: landing page, creation of component Faq,Testimonial and FeatureCard
    
    * feat: landing page, icon at the centre
    
    ---------
    
    Co-authored-by: franckniat <niatmanuel2002@gmail.com>
    Co-authored-by: Franck NIAT <97402791+franckniat@users.noreply.github.com>
    
    * Correction of buildd landing page
    
    * Dev yan (#47)
    
    * fix responsive button, landing page
    
    * feat: setting of not found page and error page for (auth)
    
    ---------
    
    Co-authored-by: franckniat <niatmanuel2002@gmail.com>
    Co-authored-by: Franck NIAT <97402791+franckniat@users.noreply.github.com>

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+   <div className="w-full h-screen flex justify-center items-center">
+    <h1 className="text-center text-slate-900 text-4xl my-6">
+      oupss 404 not found
+    </h1>
+   </div>
+  );
+}
