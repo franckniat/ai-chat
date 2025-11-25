5e9f940a149ec67df8ea83930861bac73cf17b3f
Franck NIAT <97402791+franckniat@users.noreply.github.com>
2025-11-25 23:57:44 +0100

    Feature/react email (#66) (#67)
    * feat: mettre à jour les exemples de contenu dans le composant MessagesContent
    
    * update messages component
    
    * add fotter to the main page
    
    * update prettier config
    
    * Staging (#36)
    
    * Staging (#37)
    
    * Preview (#38)
    
    * Update UI design and font (#39)
    
    * preview by yan
    
    * feat: desigining landing page, Hero, features,testimonials,Faq,CTA
    
    * feat: landing page, creation of component Faq,Testimonial and FeatureCard
    
    * feat: landing page, icon at the centre
    
    * Dev yan (#44)
    
    * feat: mettre à jour les exemples de contenu dans le composant MessagesContent
    
    * update messages component
    
    * Preview (#35)
    
    * update prettier config
    
    * Staging (#36)
    
    * Staging (#37)
    
    * Preview (#38)
    
    * Update UI design and font (#39)
    
    * feat: landing page, creation of component Faq,Testimonial and FeatureCard
    
    * feat: landing page, icon at the centre
    
    ---------
    
    
    
    
    * Correction of buildd landing page
    
    * Dev yan (#47)
    
    * fix responsive button, landing page
    
    * feat: setting of not found page and error page for (auth)
    
    * Feature/page not found (#54)
    
    * feat: mettre à jour les exemples de contenu dans le composant MessagesContent
    
    * update messages component
    
    * add fotter to the main page
    
    * update prettier config
    
    * Staging (#36)
    
    * Staging (#37)
    
    * Preview (#38)
    
    * Update UI design and font (#39)
    
    * preview by yan
    
    * feat: desigining landing page, Hero, features,testimonials,Faq,CTA
    
    * feat: landing page, creation of component Faq,Testimonial and FeatureCard
    
    * feat: landing page, icon at the centre
    
    * Dev yan (#44)
    
    * feat: mettre à jour les exemples de contenu dans le composant MessagesContent
    
    * update messages component
    
    * Preview (#35)
    
    * update prettier config
    
    * Staging (#36)
    
    * Staging (#37)
    
    * Preview (#38)
    
    * Update UI design and font (#39)
    
    * feat: landing page, creation of component Faq,Testimonial and FeatureCard
    
    * feat: landing page, icon at the centre
    
    ---------
    
    
    
    
    * Correction of buildd landing page
    
    * Dev yan (#47)
    
    * fix responsive button, landing page
    
    * feat: setting of not found page and error page for (auth)
    
    ---------
    
    
    
    
    * feat: setting of not found page and error page for (auth)
    
    * fix: center text for feature card at home and adding of Magic component
    
    * Feat: desiging of react email on reset-password and verify-email
    
    * fix: local build issue after react-email setting
    
    ---------
    
    Co-authored-by: dronex237 <161221328+dronex237@users.noreply.github.com>

---
+++ src/components/ui/divider.tsx
@@ -0,0 +1,11 @@
+import { Hr, Tailwind } from "@react-email/components";
+
+export function Divider() {
+    return (
+<Tailwind>
+  <>
+    <Hr className="my-[16px] border-gray-300 border-t-2" />
+  </>  
+</Tailwind>
+    )
+}
